1e583e9aee09eac25e09e7dbb130f1bff6ff4a3b
Jalil Abdullayev <abdullacelil00@gmail.com>
2024-11-30 10:52:46 +0400

    Datatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {AfterViewInit, Component, inject, ViewChild} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSelectModule} from '@angular/material/select';
@@ -11,6 +11,17 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatSort, MatSortModule, Sort} from '@angular/material/sort';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+
+interface PeriodicElement {
+  position: number;
+  name: string;
+  weight: number;
+  symbol: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -27,12 +38,37 @@ import {MatGridListModule} from '@angular/material/grid-list';
     RouterLink,
     MatIconModule,
     MatIconButton,
-    MatGridListModule
+    MatGridListModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  tiles: any[] = [
+    {text: 'One', cols: 3, rows: 1},
+    {text: 'Two', cols: 1, rows: 2},
+    {text: 'Three', cols: 1, rows: 1},
+    {text: 'Four', cols: 2, rows: 1}
+  ];
+
+  elements: MatTableDataSource<PeriodicElement> = new MatTableDataSource<PeriodicElement>([
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ]);
+
+  elementColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
   fb = inject(FormBuilder);
   form = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -40,10 +76,24 @@ export class AppComponent {
     dateOfBirth: ['']
   });
 
-  tiles: any[] = [
-    {text: 'One', cols: 3, rows: 1},
-    {text: 'Two', cols: 1, rows: 2},
-    {text: 'Three', cols: 1, rows: 1},
-    {text: 'Four', cols: 2, rows: 1}
-  ];
+  private liveAnnouncer: LiveAnnouncer = inject(LiveAnnouncer);
+  @ViewChild(MatSort) sort?: MatSort;
+  @ViewChild(MatPaginator) paginator?: MatPaginator;
+
+  announceSortChange(sortState: Sort): void {
+    if(sortState.direction) {
+      this.liveAnnouncer.announce(`Sorted ${sortState.direction}ending`)
+    } else {
+      this.liveAnnouncer.announce('Sorting cleared')
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if(this.sort) {
+      this.elements.sort = this.sort;
+    }
+    if(this.paginator) {
+      this.elements.paginator = this.paginator;
+    }
+  }
 }
